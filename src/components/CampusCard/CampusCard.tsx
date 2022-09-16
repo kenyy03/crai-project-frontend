311311c75d95b9a2b9d7b4de097ee4e8d3b0a184
kenyy03 <keny03@unitec.edu>
2022-09-15 19:38:35 -0600

    Redesign with material ui for CampusCard component

--- src/components/CampusCard/CampusCard.tsx
+++ src/components/CampusCard/CampusCard.tsx
@@ -2,25 +2,39 @@ import React from 'react';
 import './CampusCard.css';
 import { Link } from 'react-router-dom';
 import { Campus } from '../../interfaces/Campus';
+import { Button, Card, CardActions, CardContent } from '@mui/material';
 
-interface CampusProps  {
+interface CampusProps {
   sedes: Campus;
 }
 
-const CampusCard = ({sedes}:CampusProps) => {
+const CampusCard = ({ sedes }: CampusProps) => {
   return (
-    <div className="card">
-      <img src={require(`../../assets/${sedes.nameImg}`)} alt='Campus' className='img-card' />
-      <div className="content-card">
-        <p className="title-card">
-          <span>{sedes.campusName}</span> {sedes.campusPlace}
-        </p>
-        <p className="direction-campus-card">
-          {sedes.campusDirection}
-        </p>
-      </div>
-      <Link to={'/rooms'} className="button-card" >Reservar ahora</Link>
-    </div>
+    <>
+      <Card className="card">
+        <img
+          src={require(`../../assets/${sedes.nameImg}`)}
+          alt="Campus"
+          className="img-card"
+        />
+        <CardContent>
+          <p className="title-card">
+            <span>{sedes.campusName}</span> {sedes.campusPlace}
+          </p>
+          <p className="direction-campus-card">{sedes.campusDirection}</p>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained">
+            <Link
+              to={'/rooms'}
+              style={{ textDecoration: 'none', color: '#fff' }}
+            >
+              Reservar ahora
+            </Link>
+          </Button>
+        </CardActions>
+      </Card>
+    </>
   );
 };
 
