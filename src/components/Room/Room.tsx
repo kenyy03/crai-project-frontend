c74bd8f061c3a05f8630ebf8ff8727c028ec7ceb
Keny Travanino <keny03@unitec.edu>
2022-09-23 20:43:15 -0700

    Fixing warnings alerts

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -31,7 +31,7 @@ const Room = () => {
       }
     };
     getSedeById();
-  }, []);
+  }, [sedeId]);
 
   const getUniversityName = () => {
     const index = rooms?.campusName.indexOf(university);
